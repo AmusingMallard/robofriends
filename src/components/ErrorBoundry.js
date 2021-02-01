70930a4559eb33932667c60ce995083186d0865b
dockeith <kenneth.crimson@gmail.com>
2021-02-02 00:44:49 +0100

    refactored to use React hooks

--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import { render } from "react-dom";
 
 class ErrorBoundry extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      hasError: false
+      hasError: false,
     };
   }
 
